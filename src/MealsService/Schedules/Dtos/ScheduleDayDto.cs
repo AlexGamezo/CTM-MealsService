1a2f2f7c66987eaf6a137c0e59f388154f647e1f
Alex Gamezo <alex@agamezo.com>
2019-06-23 18:45:43 -0700

    ScheduleDayDto dates should be unspecified kind

--- src/MealsService/Schedules/Dtos/ScheduleDayDto.cs
+++ src/MealsService/Schedules/Dtos/ScheduleDayDto.cs
@@ -5,9 +5,22 @@ namespace MealsService.Responses.Schedules
 {
     public class ScheduleDayDto
     {
+        private DateTime _date;
+        private DateTime _lastModified;
+
         public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public DateTime LastModified { get; set; }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = DateTime.SpecifyKind(value, DateTimeKind.Unspecified); }
+        }
+
+        public DateTime LastModified
+        {
+            get { return _lastModified; }
+            set { _lastModified = DateTime.SpecifyKind(value, DateTimeKind.Unspecified); }
+        }
 
         public string DietType { get; set; }
         public int DietTypeId { get; set; }
